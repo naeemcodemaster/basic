e8b6ab07c421e481c5dd33b0e66757fe8515a312
= <=>
2024-10-20 22:10:32 +0500

    basic done

--- App.jsx
+++ App.jsx
@@ -1,18 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, StyleSheet, Text, TextInput, TouchableOpacity, Vibration, View } from 'react-native';
 
 export default function App() {
+  const [name,setName] = useState('');
+  console.log(name);
   return (
     <View style={styles.container}>
       <Text>Welcome to New App</Text>
-      <Text>hi</Text>
+      <Text>hi {name}</Text>
       <View style={{backgroundColor:'orange',width:'100%'}}>
-          <Text>Thi si me</Text>
+          <Text>This is me</Text>
       </View>
       <TouchableOpacity onPress={()=>console.log("h")}>
         <Text>Click Me</Text>
       </TouchableOpacity>
-      <StatusBar style="auto" />
+      <TextInput placeholder='Enter name' value={name} onChange={(e)=>setName(e.nativeEvent.text)}/>
+      <TouchableOpacity onPress={()=>Alert.alert('Congratulations','submitted',[
+        {
+          text:'Cancel',
+          onPress:()=>Alert.alert('Cancel pressed'),
+          style:'cancel'
+        }
+      ])}>
+        <Text>Submit</Text>
+      </TouchableOpacity>
+
+
+      <TouchableOpacity onPress={()=>Vibration.vibrate()} style={{backgroundColor:'green',padding:5}}>
+        <Text>Vibrate</Text>
+      </TouchableOpacity>
+      <StatusBar backgroundColor='orange' style='light' />
     </View>
   );
 }
